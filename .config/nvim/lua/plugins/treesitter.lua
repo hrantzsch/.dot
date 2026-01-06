61e6689ccd617ddaa7231bfc1c0d119ea3d8b5fa
Hannes Rantzsch <mail@hannesrantzsch.de>
2026-01-06 21:22:57 +0100

    nvim: fixes after treesitter update

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
     build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "bash", "c", "cpp", "haskell", "html", "javascript", "lua", "python", "rust",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
@@ -19,9 +10,9 @@ return {
     -- TODO: map require("treesitter-context").go_to_context()
     opts = { max_lines = 4, mode = 'topline', },
   },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    -- TODO: setup custom text objects
-    depenencies = { "nvim-treesitter/nvim-treesitter" },
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter-textobjects",
+  --   -- TODO: setup custom text objects
+  --   depenencies = { "nvim-treesitter/nvim-treesitter" },
+  -- },
 }
