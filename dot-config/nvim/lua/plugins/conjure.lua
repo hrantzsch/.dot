b401aef2319b99053c68450a6d57ace3610eb792
Hannes Rantzsch <hannes.rantzsch@checkmk.com>
2026-08-16 15:39:55 +0200

    nvim: conjure option

--- dot-config/nvim/lua/plugins/conjure.lua
+++ dot-config/nvim/lua/plugins/conjure.lua
@@ -6,6 +6,11 @@ return {
     lazy = true,
     config = function()
       vim.g["conjure#mapping#doc_word"] = {"<LocalLeader>k"}
+      vim.g["conjure#log#hud#width"] = 0.8
+      vim.g["conjure#log#hud#height"] = 0.4
+      vim.g["conjure#log#hud#open_when"] = "log-win-not-visible"
+      vim.g["conjure#highlight#enabled"] = true
+      vim.g["conjure#highlight#timeout"] = 300
     end
   }
 }
